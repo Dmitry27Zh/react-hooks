9cb3e358b207958cf9758f3e9bdf5ac87e0364ee
Dmitry <dmitry27zh@gmail.com>
2023-09-09 20:05:55 +0300

    Add useEffect to renderCount

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -2,17 +2,19 @@ import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
-    const someRef = useRef("string");
-    const [renderCount, setRenderCount] = useState(0);
+    const renderCount = useRef(0);
+    const [otherState, setOtherState] = useState(false);
     useEffect(() => {
-        setRenderCount((prevState) => prevState + 1);
+        renderCount.current++;
     });
-
+    const toggleOtherState = () => {
+        setOtherState(!otherState);
+    };
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
-            {someRef.current}
-            <p>RenderCount: {renderCount}</p>
+            <p>RenderCount: {renderCount.current}</p>
+            <button className="btn btn-primary" type="button" onClick={toggleOtherState}>Toggle other state</button>
         </CardWrapper>
     );
 };
