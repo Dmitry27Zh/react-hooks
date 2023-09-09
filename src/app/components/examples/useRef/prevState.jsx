19d121ac10ec873d18f8ecf3b0ba0de4aac3c3da
Dmitry <dmitry27zh@gmail.com>
2023-09-09 20:21:29 +0300

    useRef to save prevState

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+
 const PrevStateExample = () => {
+    const [someState, setSomeState] = useState("false");
+    const previousState = useRef("");
+    const toggleSomeState = () => {
+        setSomeState((prevState) => prevState === "false" ? "true" : "false");
+    };
+    useEffect(() => {
+        previousState.current = someState;
+    }, [someState]);
+
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <p>Some state: {someState}</p>
+            <p>Previous state: {previousState.current}</p>
+            <button className="btn btn-primary" type="button" onClick={toggleSomeState}>Toggle some state</button>
         </CardWrapper>
     );
 };
