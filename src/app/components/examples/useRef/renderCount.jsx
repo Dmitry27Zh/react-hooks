9cb389abf51353fbf257834d1582e0349dd03e52
Dmitry <dmitry27zh@gmail.com>
2023-09-09 19:59:34 +0300

    Start renderCount: try useState, infinite loop

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
+    const someRef = useRef("string");
+    const [renderCount, setRenderCount] = useState(0);
+    useEffect(() => {
+        setRenderCount((prevState) => prevState + 1);
+    });
+
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            {someRef.current}
+            <p>RenderCount: {renderCount}</p>
         </CardWrapper>
     );
 };
