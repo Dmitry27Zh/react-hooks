9a6609251333f8c5240aa4c64bb54301cb4bf46c
Dmitry <dmitry27zh@gmail.com>
2023-09-09 20:35:04 +0300

    useRef to save element

--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 import Divider from "../../common/divider";
 
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+    const handleClick = () => {
+       inputRef.current.focus();
+    };
+
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
             <Divider />
+            <label className="form-label" htmlFor="email">Email</label>
+            <input className="form-control" ref={inputRef} type="email" name="email" id="email" />
+            <button className="btn btn-primary" type="button" onClick={handleClick}>Focus input</button>
         </CardWrapper>
     );
 };
